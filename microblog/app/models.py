971d6e29fd41efcf2d44fab371313ee4a0a3defd
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2018-09-15 02:14:00 +0200

    Added edit profile form
    Change-Id: I1cabb53006ea27797f08d03243a0b4794777c5be

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -4,9 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db, login
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,6 +27,10 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
